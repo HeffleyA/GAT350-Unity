fcaa831e9415b062c46d009e60503d937d872a90
HeffleyA <aheffley@student.neumont.edu>
2024-11-18 18:32:58 -0700

    First Shader

--- Assets/Scripts/MaterialController.cs
+++ Assets/Scripts/MaterialController.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class MaterialController : MonoBehaviour
 {
+    [Range(0.1f, 0.5f)] public float bloat;
+    public MeshRenderer meshRenderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        meshRenderer.material.SetFloat("_Bloat", bloat);
     }
 }
